ae36d656bc716fcd07757dca2c4098ce775856b3
Daniel Ly <nalply@gmail.com>
2011-08-16 16:32:47 +0200

    Make Hello.js really use the configuration

--- views/Hello.js
+++ views/Hello.js
@@ -6,7 +6,7 @@ function hello(task, cb) {
   var DBG = task.config.DBG;
   
   task.response.writeHead(200, {'Content-Type': 'text/plain'});
-  task.response.end('Hello World!\n');
+  task.response.end(task.config.views.Hello.title + "\n");
   
   console.log("hello did its work"); // Should be a logger step
   task.end(cb);
